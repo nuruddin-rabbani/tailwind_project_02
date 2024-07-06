53916a26af0f7e0c9b901a5ceaba52bcd776485b
nuruddin-rabbani <sadinbangla79@gmail.com>
2024-07-06 11:07:49 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    darkMode: "class",
+    content: ["*"],
+    theme: {
+        extend: {
+            backgroundImage: (theme) => ({
+                "testimonial-image":
+                    "url('/images/brooke-cagle-g1Kr4Ozfoac-unsplash 2.png')",
+            }),
+        },
+    },
+    plugins: [],
+};
